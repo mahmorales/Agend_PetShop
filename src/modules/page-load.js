cbe66b5fb77abe999b0678839e066f943ddd6483
Maisa Morales <maisa.morales8@gmail.com>
2026-09-08 19:36:25 -0300

    Refactor date input assignment for clarity and consistency

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -1,11 +1,12 @@
 import dayjs from "dayjs"
-import { schedulesDay  } from "./schedules/load.js"
+import { schedulesDay } from "./schedules/load.js"
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Define a data atual no input
-  const currentDate = document.getElementById("schedule-date")
   const today = dayjs().format("YYYY-MM-DD")
-  currentDate.value = today
+  
+  // Define a data atual nos dois inputs (topo da página e dentro do modal)
+  document.getElementById("schedule-date").value = today
+  document.getElementById("date").value = today
 
   // Carrega os agendamentos do dia atual
   schedulesDay()
