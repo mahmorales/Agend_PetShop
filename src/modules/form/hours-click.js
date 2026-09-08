ca6fd0843a602c846bcc774e54c656b3745363b1
Maisa Morales <maisa.morales8@gmail.com>
2026-09-08 19:36:40 -0300

    Fix hours selection logic by correcting method from items to forEach

--- src/modules/form/hours-click.js
+++ src/modules/form/hours-click.js
@@ -1,7 +1,7 @@
 export function hoursClick() {
   const hours = document.querySelectorAll(".hour-available")
 
-  hours.items.forEach((availableHour) => {
+  hours.forEach((availableHour) => {
     availableHour.addEventListener("click", (selected) => {
       // Remove a classe 'selected' de todos e adiciona apenas no clicado
       hours.forEach((hour) => {
