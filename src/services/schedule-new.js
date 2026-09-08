6c1203419c48dfc4fe467a2a7e5ad49e9a4687bf
Maisa Morales <maisa.morales8@gmail.com>
2026-09-08 17:25:13 -0300

    Add scheduleNew function to handle new appointment scheduling

---
+++ src/services/schedule-new.js
@@ -0,0 +1,19 @@
+import { apiConfig } from "./api-config.js"
+
+export async function scheduleNew({ id, name, pet, phone, when}) {  
+  try {
+    await fetch(`${apiConfig.baseURL}/schedules`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id, name, pet, phone, when }),
+    }) 
+    
+    alert("Agendamento realizado com sucesso.")
+    
+  } catch (error) {
+    console.log(error)
+    alert("Não foi possível realizar o agendamento. Tente novamente.")
+  }
+}
