c1b675378326f2f33c8c766785c7b7807963337c
Maisa Morales <maisa.morales8@gmail.com>
2026-09-08 16:52:05 -0300

    Add main.js file with Babel configuration and CSS imports

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Configuração do Babel / JS moderno
+"use strict"
+
+// Importação dos estilos CSS
+import "./styles/global.css"
+import "./styles/header.css"
+import "./styles/schedule.css"
+import "./styles/modal.css"
+
+console.log("PetShop conectado com o Webpack do padrão HairDay!")
