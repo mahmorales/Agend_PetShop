d25d7c30fac97154623ced496aec64d88d0b3792
Maisa Morales <maisa.morales8@gmail.com>
2026-09-08 17:51:21 -0300

    Add hoursClick function to handle hour selection in appointment scheduling

---
+++ src/modules/form/hours-click.js
@@ -0,0 +1,14 @@
+export function hoursClick() {
+  const hours = document.querySelectorAll(".hour-available")
+
+  hours.items.forEach((availableHour) => {
+    availableHour.addEventListener("click", (selected) => {
+      // Remove a classe 'selected' de todos e adiciona apenas no clicado
+      hours.forEach((hour) => {
+        hour.classList.remove("selected")
+      })
+
+      selected.target.classList.add("selected")
+    })
+  })
+}
