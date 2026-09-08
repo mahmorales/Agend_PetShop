a6ef3d60bbbd1922a8048ada925df2d23bbd92bf
Maisa Morales <maisa.morales8@gmail.com>
2026-09-08 17:35:01 -0300

    Add imports for dayjs library and page load module

--- src/main.js
+++ src/main.js
@@ -7,4 +7,10 @@ import "./styles/header.css"
 import "./styles/schedule.css"
 import "./styles/modal.css"
 
+//importação do dayjs
+import "./libs/dayjs.js"
+
+//importação do carregamento da página
+import "./modules/page-load.js"
+
 console.log("PetShop conectado com o Webpack do padrão HairDay!")
